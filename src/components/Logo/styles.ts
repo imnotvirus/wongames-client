d09af8265f8070bb42b2382e219731ae378ff0c9
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-06-02 17:08:41 -0300

    hide text on mobile props

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 import { LogoProps } from '.'
-
 const wrapperModifiers = {
   normal: () => css`
     width: 11rem;
@@ -9,12 +9,27 @@ const wrapperModifiers = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+    width: 5.8rem;
+    height: 4.5rem;
+
+    svg {
+      height: 4.5rem;
+      pointer-event: 'none';
+    }
+    .logo-text {
+      display: none;
+    }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
-    ${!!size && wrapperModifiers[size]()}
+    ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile()}
   `}
 `
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -23,10 +23,22 @@ describe('<Logo />', () => {
       width: '20rem'
     })
   })
+
   it('should render a normal logo by default', () => {
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '11rem'
     })
+  })
+
+  it('should render a big logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
