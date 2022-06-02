573afc3e2036b170449b0fe13a463b034ebf2b22
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-06-02 16:25:20 -0300

    add size atributes to logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -16,4 +16,17 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render a big logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+  it('should render a normal logo by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
 })
