5b526367163a7e2a6d6e8e0777c78d5bc17b15a4
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-06-02 15:43:23 -0300

    pass props on storybook logo

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,9 @@
-import { Story, Meta } from '@storybook/react'
-import Logo from '.'
+import { Meta, Story } from '@storybook/react'
+import Logo, { LogoProps } from '.'
 
 export default {
   title: 'Logo',
   component: Logo
 } as Meta
 
-export const Default: Story = () => <Logo />
+export const Default: Story<LogoProps> = (props) => <Logo {...props} />
